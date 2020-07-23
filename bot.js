abe67412017dd79cfa05440f4a436ef738c83b10
syndromev <68643908+syndromev@users.noreply.github.com>
2020-07-23 21:54:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'bye') {
+    	message.reply('Bye, also I like to destroy things.');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
