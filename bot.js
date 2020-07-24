cc310e07460892c40aaecac1ecdbcf66b8f56f9e
syndromev <68643908+syndromev@users.noreply.github.com>
2020-07-24 11:47:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,6 @@ client.on('message', message => {
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
-        status: "online",  //You can show online, idle....
         game: {
             name: "Using !help",  //The message shown
             type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
