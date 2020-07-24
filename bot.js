f1f5b281a453bd2f18eee1577ddd22f151c9ce69
syndromev <68643908+syndromev@users.noreply.github.com>
2020-07-24 11:42:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,14 +17,8 @@ client.on('message', message => {
   	}
 });
 
-static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.details = "♡";
-    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-    Discord_UpdatePresence(&discordPresence);
-}
+client.user.setActivity(“over family”, { type: “WATCHING” });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
